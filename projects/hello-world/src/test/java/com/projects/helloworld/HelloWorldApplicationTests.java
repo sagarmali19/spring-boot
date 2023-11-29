94ed4aa6c2e9534bc552df4d152ca2f817bcf921
sagarmali19 <sagarmali.work@gmail.com>
2023-11-30 00:53:59 +0530

    Initial setup for the spring boot project

---
+++ projects/hello-world/src/test/java/com/projects/helloworld/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.projects.helloworld;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HelloWorldApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
